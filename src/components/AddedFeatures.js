174e2ffc2263c1f9e97def0ed4ecfdb4c1d707e5
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-02-06 18:33:03 -0500

    MVP reached. A little wonky when removing features but it does at it should..........

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 import AddedFeature from './AddedFeature';
+import { useSelector } from "react-redux"
 
 const AddedFeatures = props => {
+  const car = useSelector(state => state.car)
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {car.features.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
+          {car.features.map(item => (
             <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
-        <p>You can purchase items from the store.</p>
-      )}
+          <p>You can purchase items from the store.</p>
+        )}
     </div>
   );
 };
